c76af510e633a01b6f438b0b473c7075e7faa7d0
Himanshu Maurya <113373335+himanshumaurya0007@users.noreply.github.com>
2024-02-04 20:45:25 +0530

    Final project

--- MedIQ_Advisor/MedIQ_Advisor_App/admin.py
+++ MedIQ_Advisor/MedIQ_Advisor_App/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from MedIQ_Advisor_App.models import Contact,Sign_up,Mental_Health_Survey
+admin.site.register(Contact)
+admin.site.register(Sign_up)
+admin.site.register(Mental_Health_Survey)
 
 # Register your models here.
